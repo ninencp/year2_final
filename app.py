3af9cab496eb516b274d25e324697671d32c653d
ninencp <64160216@go.buu.ac.th>
2023-03-20 01:13:53 +0700

    std : edit route & page

--- app.py
+++ app.py
@@ -296,15 +296,17 @@ def UpdateStd(id):
     data = session['data']
 
     if 'loggedin' in session and 'std' in session:
-        if request.method == 'POST' and 'username' in request.form and 'std_name' in request.form and 'std_id' in request.form:
+        if request.method == 'POST' and 'username' in request.form and 'std_name' in request.form and 'new_std_id' in request.form:
             name = request.form['std_name']
             username = request.form['username']
+            new_std_id = request.form['new_std_id']
             cursor.execute("\
                         UPDATE teacher\
                         SET std_name = %s,\
-                            username = %s\
+                            username = %s,\
+                            std_id = %s\
                         WHERE std_id = %s\
-                        ", (name, username, id))
+                        ", (name, username, id, new_std_id))
             db.commit()
             return redirect(url_for('Login'))
         elif request.method == 'POST' and 'password' in request.form and 'conf_pw' in request.form:
@@ -313,7 +315,7 @@ def UpdateStd(id):
 
             if password == conf_pw:
                 cursor.execute("\
-                            UPDATE teacher\
+                            UPDATE student\
                             SET password = %s,\
                                 conf_password = %s\
                             WHERE teacher_id = %s\
