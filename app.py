e026990d0c40fcecf94440c5085c8fa49c10fc1f
ninencp <64160216@go.buu.ac.th>
2023-03-19 10:50:13 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,151 @@
+import pymysql, re
+
+from flask import (Flask, flash, redirect, render_template, request, session,
+                   url_for)
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+app.secret_key = 'buu-iot'
+
+mysql = MySQL()
+
+#MySQL Configuration
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_DB'] = 'iot_project'
+mysql.init_app(app)
+
+# -------------------------- Content --------------------------
+
+# @app.route("/home")
+# def Index():
+#     db = mysql.connect()
+#     cursor = db.cursor(pymysql.cursors.DictCursor)
+#     # Check if user is logged in
+#     if 'loggedin' in session:
+#         cursor.execute("SELECT * FROM course INNER JOIN session ON session.course_id=course.course_id")
+#         data = cursor.fetchone()
+#         print(data)
+#         # if user logged in show them homepage
+#         return render_template('index.html', student_id=session['student_id'], student_name=session['name'], data=data)
+#     # if user isn't logged in return to login page
+#     return redirect(url_for('Login'))
+
+@app.route("/register_std", methods=['GET', 'POST'])
+def Register_std():
+    db = mysql.connect()
+    cursor = db.cursor(pymysql.cursors.DictCursor)
+
+    msg = ''
+
+    # if username & password POST request exist
+    if request.method == "POST" and 'pw' in request.form and 'conf_pw' in request.form and 'student_id' in request.form:
+        # Create variable for easy access
+        student_id = request.form['student_id']
+        password = request.form['pw']
+        conf_pw = request.form['conf_pw']
+        name = request.form['name']
+
+        print(student_id, password, name)
+
+        # Check if account exist in MySQL
+        cursor.execute("SELECT * from student WHERE std_id = %s", (student_id,))
+        # Fetch one record
+        account = cursor.fetchone()
+
+        if account:
+            msg = 'Account already exists'
+        elif not student_id or not name or not password:
+            msg = 'Please fill out the form'
+        elif not re.match(r'[0-9]+', student_id):
+            msg = 'Invalid student id'
+        elif not password == conf_pw:
+            msg = 'Password did not match'
+        else:
+            # Account does not exist
+            cursor.execute("INSERT INTO student (std_id, std_name, password, conf_password) VALUES (%s, %s, %s, %s)", (student_id, name, password, conf_pw))
+            db.commit()
+            msg = 'Successfully Registered'
+
+    elif request.method == "POST":
+        msg = 'Please fill out the form'
+
+    return render_template("register_std.html", msg=msg)
+
+@app.route("/register_teacher", methods=['GET', 'POST'])
+def Register_teacher():
+    db = mysql.connect()
+    cursor = db.cursor(pymysql.cursors.DictCursor)
+
+    msg = ''
+
+    # if username & password POST request exist
+    if request.method == "POST" and 'pw' in request.form and 'conf_pw' in request.form and 'username' in request.form:
+        # Create variable for easy access
+        username = request.form['username']
+        password = request.form['pw']
+        conf_pw = request.form['conf_pw']
+        name = request.form['name']
+
+        print(username, password, name)
+
+        # Check if account exist in MySQL
+        cursor.execute("SELECT * from student WHERE std_id = %s", (username,))
+        # Fetch one record
+        account = cursor.fetchone()
+
+        if account:
+            msg = 'Account already exists'
+        elif not username or not name or not password:
+            msg = 'Please fill out the form'
+        elif not password == conf_pw:
+            msg = 'Password did not match'
+        else:
+            # Account does not exist
+            cursor.execute("INSERT INTO teacher (teacher_name, username, password, conf_password) VALUES (%s, %s, %s, %s)", (name, username, password, conf_pw))
+            db.commit()
+            msg = 'Successfully Registered'
+
+    elif request.method == "POST":
+        msg = 'Please fill out the form'
+
+    return render_template("register_teacher.html", msg=msg)
+
+@app.route("/", methods=['GET', 'POST'])
+def Login():
+    db = mysql.connect()
+    cursor = db.cursor(pymysql.cursors.DictCursor)
+
+    msg = ''
+
+    # If "username" and "password" POST requests exist (user submitted form)
+    if request.method == "POST" and 'student_id' in request.form and 'pw' in request.form :
+        # Create variable
+        student_id = request.form['student_id']
+        pw = request.form['pw']
+        # Check if accounts exist in MySQL
+        cursor.execute("SELECT * FROM student WHERE student_id = %s AND password = %s", (student_id, pw))
+        # Fetch one record
+        account = cursor.fetchone()
+        print(account)
+
+        if account:
+            # Create session data to access in other route
+            session['loggedin'] = True
+            session['student_id'] = account['student_id']
+            session['name'] = account['name']
+            # Redirect to index page
+            return redirect((url_for('Index')))
+        else:
+            msg = 'Incorrect student id or password'
+
+    return render_template("login.html", msg=msg)
+
+# ---------------- teacher section ---------------- #
+
+# @app.route("/teacher/index")
+
+# start app
+if __name__ == "__main__":
+    app.run(port=4000, debug=True)
---
+++ db/db.py
@@ -0,0 +1,80 @@
+import mysql.connector
+import datetime
+
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='',
+    db='iot_project'
+)
+
+mycursor = mydb.cursor()
+
+# mycursor.execute("CREATE DATABASE iot_project")
+
+# mycursor.execute("CREATE TABLE student \
+#                  (\
+#                  std_id int NOT NULL PRIMARY KEY,\
+#                  std_name varchar(50) NOT NULL,\
+#                  username varchar(50) NOT NULL,\
+#                  password varchar(50) NOT NULL,\
+#                  conf_password varchar(50) NOT NULL\
+#                  )")
+
+# mycursor.execute("CREATE TABLE teacher (\
+#                  teacher_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,\
+#                  teacher_name varchar(50) NOT NULL,\
+#                  username varchar(50) NOT NULL,\
+#                  password varchar(50) NOT NULL,\
+#                  conf_password varchar(50) NOT NULL\
+#                  )")
+
+# mycursor.execute("CREATE TABLE subject (\
+#                  s_id int NOT NULL PRIMARY KEY,\
+#                  s_name varchar(100),\
+#                  start_time datetime NOT NULL,\
+#                  end_time datetime NOT NULL,\
+#                  ref_teacher_id int NOT NULL COMMENT 'id อาจารย์',\
+#                  FOREIGN KEY (ref_teacher_id) REFERENCES teacher(teacher_id) \
+#                  )")
+
+# mycursor.execute("CREATE TABLE enroll (\
+#                  enroll_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,\
+#                  ref_s_id int NOT NULL COMMENT 'id วิชา',\
+#                  FOREIGN KEY (ref_s_id) REFERENCES subject(s_id) ,\
+#                  ref_std_id int NOT NULL COMMENT 'id นิสิต', \
+#                  FOREIGN KEY (ref_std_id) REFERENCES student(std_id) \
+#                  )")
+
+# mycursor.execute("CREATE TABLE checkin (\
+#                  no int PRIMARY KEY NOT NULL AUTO_INCREMENT,\
+#                  ref_teacher_id int NOT NULL COMMENT 'id อาจารย์',\
+#                  FOREIGN KEY (ref_teacher_id) REFERENCES teacher(teacher_id),\
+#                  ref_s_id int NOT NULL COMMENT 'id วิชา',\
+#                  FOREIGN KEY (ref_s_id) REFERENCES subject(s_id),\
+#                  ref_std_id int NOT NULL COMMENT 'id นิสิต',\
+#                  FOREIGN KEY (ref_std_id) REFERENCES student(std_id),\
+#                  check_in_status int(1) NOT NULL COMMENT '0 ขาด 1 มา 2 สาย',\
+#                  check_in_date timestamp NOT NULL,\
+#                  date_save timestamp NOT NULL DEFAULT current_timestamp()\
+#                  )")
+
+# Insert data to session
+# today = datetime.date.today()
+# date_time = datetime.datetime.now()
+# time_change = datetime.timedelta(hours=2,minutes=50)
+# new = date_time + time_change
+
+# start = date_time.strftime("%H:%M:%S")
+# end = new.strftime("%H:%M:%S")
+# print(today, start, end)
+
+# sql = "INSERT INTO session (date, start_time, end_time, course_id) VALUES (%s, %s, %s, %s)"
+# val = [(today,start,end,89022464)]
+# mycursor.executemany(sql,val)
+
+# mycursor.execute("INSERT INTO teacher (name, email, password, conf_password) VALUES ('สมชาย ใจดี', 'somchai@mail.com', 123456, 123456)")
+
+# mycursor.execute("INSERT INTO course (course_id, name, teacher_id) VALUES(89022464, 'Internet of Things', 1)")
+
+mydb.commit()
